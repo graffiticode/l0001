6ec64d3bbf8d2588f5bdd452a85db3a6550efa4e
Jeff Dyer <jeffdyer@acm.org>
2022-12-11 09:14:17 -0800

    Make form render svg

--- src/pages/[type].jsx
+++ src/pages/[type].jsx
@@ -11,7 +11,7 @@ function renderAttr(attr) {
   return attr;
 }
 
-function renderElts(data) {
+function render(data) {
   data = [].concat(data);
   const elts = [];
   let key = 1;
@@ -21,10 +21,10 @@ function renderElts(data) {
     }
     switch(d.type) {
     case 'b':
-      elts.push(<b key={key++}>{renderElts(d.elts)}</b>);
+      elts.push(<b key={key++}>{render(d.elts)}</b>);
       break;
     default:
-      elts.push(d);
+      elts.push(<text x="10" y="50">{d}</text>);
       break;
     }
   });
@@ -32,23 +32,40 @@ function renderElts(data) {
 }
 
 const Form = () => {
+  const [ elts, setElts ] = useState([]);
   const router = useRouter();
   const { type, data } = router.query;
-  const [ elts, setElts ] = useState([]);
   useEffect(() => {
-    try {
-      if (data) {
-        setElts(renderElts(JSON.parse(data)));
-      }
-    } catch (x) {
-      // Bad data.
-      console.log("Bad data in query: " + x);
+    if (data === undefined) {
+      return;
     }
+    const { url } = JSON.parse(data);
+    console.log("Form() url=" + url);
+    (async () => {
+      const resp = await fetch(
+        url,
+        { headers: {'Content-Type': 'application/json'}}
+      );
+      const { data } = await resp.json();
+      console.log("Form() data=" + JSON.stringify(data));
+      if (data === undefined) {
+        return;
+      }
+      try {
+        setElts(render(data));
+      } catch (x) {
+        // Bad data.
+        console.log("Bad data in query: " + x);
+      }
+    })();
   }, [data]);
+
   return (
-    <div>
+    <div id="graffiti">
       <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
-      {elts}
+      <svg width="100%" height="100">
+        {elts}
+      </svg>
     </div>
   );
 }
