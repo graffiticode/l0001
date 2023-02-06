86555033b30106099a5899f19da7926dee053307
Kevin Dyer <kevin.m.dyer@gmail.com>
2023-02-06 09:16:44 -0700

    fix: await for compile to finish before returning

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,30 +1,33 @@
 import { compiler } from './compiler.js';
-export default function compileHandler(req, res) {
+export default async function compileHandler(req, res) {
   const {
     method,
     body,
   } = req;
   const { code, data, config } = body;
   switch (method) {
-  case 'POST':      
-    let body = req.body;
-    let code = body.code || body.src;
-    let data = body.data;
-    let config = body.config || {};
-    if (!code || !data) {
-      return res.sendStatus(400);
-    }
-    compiler.compile(code, data, config, function (err, data) {
-      if (err && err.length) {
-        res.status(500).json({error: err});
-        return;
+    case 'POST':
+      let body = req.body;
+      let code = body.code || body.src;
+      let data = body.data;
+      let config = body.config || {};
+      if (!code || !data) {
+        return res.status(400).send();
       }
-      res.status(200).json(data);
-    });
-    break;
-  default:
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${method} Not Allowed`);
-    break;
+      await new Promise((resolve) =>
+        compiler.compile(code, data, config, function (err, data) {
+          if (err && err.length) {
+            res.status(500).json({ error: err });
+          } else {
+            res.status(200).json(data);
+          }
+          resolve();
+        })
+      );
+      break;
+    default:
+      res.setHeader('Allow', ['POST']);
+      res.status(405).end(`Method ${method} Not Allowed`);
+      break;
   }
 }
