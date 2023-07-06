5f1e7ed31b2ab39c6ec74cc60de8e8b02f6e8083
Jeff Dyer <jeffdyer@acm.org>
2023-07-06 13:29:31 -0700

    Support code as non-AST data

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -6,28 +6,33 @@ export default async function compileHandler(req, res) {
   } = req;
   const { code, data, config } = body;
   switch (method) {
-    case 'POST':
-      let body = req.body;
-      let code = body.code || body.src;
-      let data = body.data;
-      let config = body.config || {};
-      if (!code || !data) {
-        return res.status(400).send();
-      }
-      await new Promise((resolve) =>
-        compiler.compile(code, data, config, function (err, data) {
-          if (err && err.length) {
-            res.status(500).json({ error: err });
-          } else {
-            res.status(200).json(data);
-          }
-          resolve();
-        })
-      );
-      break;
-    default:
-      res.setHeader('Allow', ['POST']);
-      res.status(405).end(`Method ${method} Not Allowed`);
-      break;
+  case 'POST':
+    let body = req.body;
+    let code = body.code || body.src;
+    let data = body.data;
+    let config = body.config || {};
+    if (!code || !data) {
+      return res.status(400).send();
+    }
+    if (code.root === undefined) {
+      // Special case of code as data.
+      return res.status(200).json(code);
+    }
+    await new Promise((resolve) =>
+      compiler.compile(code, data, config, function (err, data) {
+        console.log("POST /compile data=" + JSON.stringify(data, null, 2));
+        if (err && err.length) {
+          res.status(500).json({ error: err });
+        } else {
+          res.status(200).json(data);
+        }
+        resolve();
+      })
+    );
+    break;
+  default:
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${method} Not Allowed`);
+    break;
   }
 }
